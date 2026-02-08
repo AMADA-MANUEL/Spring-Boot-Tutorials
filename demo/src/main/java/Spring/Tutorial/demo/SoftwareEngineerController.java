d135f9f408c86a9d266521ff917febcd3298c893
AMADA-MANUEL <manuelamada707@gmail.com>
2026-02-08 23:39:13 +0200

    commit

--- demo/src/main/java/Spring/Tutorial/demo/SoftwareEngineerController.java
+++ demo/src/main/java/Spring/Tutorial/demo/SoftwareEngineerController.java
@@ -1,13 +1,15 @@
 package Spring.Tutorial.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("SoftwareEnigneer")
+@RequestMapping("v1/SoftwareEngineer")
 public class SoftwareEngineerController {
+    @GetMapping
     public List<SoftwareEngineer> getEngineers(){
         return List.of(
                 new SoftwareEngineer(
