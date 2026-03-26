5f3b69c8281393bb3ce329b19f80bb2fe04b1678
ammanuejhb024 <manuelamada707@gmail.com>
2026-03-26 08:25:23 +0200

    variables

---
+++ MovieAPI/src/main/java/com.movieflix/entities/Movie.java
@@ -0,0 +1,22 @@
+package com.movieflix.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Set;
+
+@Entity
+public class Movie {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Integer movieId;
+    private String title;
+    private String director;
+    private String studio;
+    private Set<String> movieCast ;
+    private Integer releaseYear;
+    private String poster;
+}
