d0ce82eca6633f71059db870fa9384f97dae66eb
ammanuejhb024 <manuelamada707@gmail.com>
2026-03-26 08:39:19 +0200

    variables

--- MovieAPI/src/main/java/com.movieflix/entities/Movie.java
+++ MovieAPI/src/main/java/com.movieflix/entities/Movie.java
@@ -1,22 +1,49 @@
 package com.movieflix.entities;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
 @Entity
+@NoArgsConstructor
+@Getter
 public class Movie {
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
+
     private Integer movieId;
+    @Column(nullable = false,length = 200 )
+    @NotBlank(message   = "Please provide movies title")
+
     private String title;
+    @Column(nullable = false )
+    @NotBlank(message   = "Please provide movies title")
+
     private String director;
+    @Column(nullable = false )
+    @NotBlank(message   = "Please provide movies director")
+
     private String studio;
+    @Column(nullable = false)
+    @NotBlank(message   = "Please provide movies studio")
+
+
+    @ElementCollection
+    @CollectionTable(name = "movie_cast")
     private Set<String> movieCast ;
+
+    @Column(nullable = false )
+    @NotBlank(message   = "Please provide movies title")
     private Integer releaseYear;
+
+    @Column(nullable = false )
+    @NotBlank(message   = "Please provide movies title")
+
     private String poster;
+//    @Column(nullable = false )
+//    @NotBlank(message   = "Please provide movies poster")
 }
