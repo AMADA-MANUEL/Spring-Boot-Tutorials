59ce25f8cfb35dafc607ba5939ff59eb7db572b6
ammanuejhb024 <manuelamada707@gmail.com>
2026-03-27 14:34:30 +0200

    database file upload

--- MovieAPI/src/main/java/service/FileServiceImpl.java
+++ MovieAPI/src/main/java/service/FileServiceImpl.java
@@ -3,9 +3,13 @@ package service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 
 @Service
@@ -14,8 +18,22 @@ public class FileServiceImpl implements FileService{
 
     @Override
     public String uploadFile(String path, MultipartFile file) throws IOException {
-        return "";
-    }
+        //get name of the file
+        String fileName = file.getOriginalFilename();
+//to get the file  pth
+        String  filePath= path + File.separator + fileName;
+//create a file object
+        File f = new File(path);
+        if(!f.exists()){
+            f.mkdir();
+        }
+        //copy the file to the path
+        Files.copy(file.getInputStream(), Paths.get(filePath) , StandardCopyOption.REPLACE_EXISTING);
+
+
+        return fileName;
+        }
+
 
     @Override
     public InputStream getResourceFile(String path, String name) throws FileNotFoundException {
