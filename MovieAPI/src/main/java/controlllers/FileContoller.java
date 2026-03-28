845406419c1ec6e4406b7e005705c7c397e08770
ammanuejhb024 <manuelamada707@gmail.com>
2026-03-28 08:04:54 +0200

    database file upload response

--- MovieAPI/src/main/java/controlllers/FileContoller.java
+++ MovieAPI/src/main/java/controlllers/FileContoller.java
@@ -14,7 +14,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
-@RestController
+@RestController //this class handles HTTP requests (API endpoints)
 @RequestMapping("/file/")
 public class FileContoller {
     private final FileService fileservice ;
@@ -23,20 +23,21 @@ public class FileContoller {
         this.fileservice = fileservice;
     }
 
-    @Value("${projects.poster}")
+    @Value("${projects.poster}") //reads the path from the resource file
     private String path ;
     @PostMapping("/upload")
     public ResponseEntity<String> uploadFileHandler(@RequestPart MultipartFile file) throws IOException{
-        String uploadedFileName =fileservice.uploadFile(path,file);
+        String uploadedFileName =fileservice.uploadFile(path,file);//Calls your service layer   Saves the file
+
         return ResponseEntity.ok("File uploaded :" + uploadedFileName);
 
     }
     @GetMapping("/{filename}")
     public void serverFileHandler(@PathVariable String filename , HttpServletResponse response) throws IOException {
         InputStream resourceFile = fileservice
-                .getResourceFile(path , filename);
-        response.setContentType(MediaType.IMAGE_PNG_VALUE);
-        StreamUtils.copy(resourceFile,response.getOutputStream());
+                .getResourceFile(path , filename); //Get file from service
+        response.setContentType(MediaType.IMAGE_PNG_VALUE); //This forces everything to be treated as PNG
+        StreamUtils.copy(resourceFile,response.getOutputStream()); //This streams the file directly to the client
 
     }
 
