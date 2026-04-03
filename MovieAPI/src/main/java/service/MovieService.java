d455504e77065b53e4ba6f8e81d4b0c3849e27b8
ammanuejhb024 <manuelamada707@gmail.com>
2026-04-03 22:54:08 +0200

    new class to interface

--- MovieAPI/src/main/java/service/MovieService.java
+++ MovieAPI/src/main/java/service/MovieService.java
@@ -1,4 +1,8 @@
 package service;
 
+import dto.Moviedto;
+import org.springframework.web.multipart.MultipartFile;
+
 public interface MovieService {
+    Moviedto addMovie(Moviedto moviedto , MultipartFile file);
 }
